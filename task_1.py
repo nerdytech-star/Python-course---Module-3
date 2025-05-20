0c7c0232ca694040c56589c85ef2dfda85ab6e2f
nerdytech-star <khushiakf88@gmail.com>
2025-05-20 12:52:37 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter first number: "))
+if a%2==0:
+    print(f"{a} is  an even number")
+else:
+    print(f"{a} is an odd number")
+# This code takes an integer input from the user and checks if it is even or odd. It prints a message indicating whether the number is even or odd.
